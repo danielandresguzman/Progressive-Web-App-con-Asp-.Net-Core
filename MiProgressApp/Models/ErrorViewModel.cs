0d92e8b16fd3c3ac9238506a935a9b6855d308d1
Daniel Guzman <dguzman@creasys.cl>
2022-07-06 19:17:16 -0400

    commit inicial con menu

---
+++ MiProgressApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiProgressApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
