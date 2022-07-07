be9cff2af50467b2a8719537f5afec9f1626a19b
Daniel Guzman <dguzman@creasys.cl>
2022-07-07 19:25:42 -0400

    inyectar tipolibro

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,9 +1,12 @@
-﻿var nombreCacheEstatico = "cacheEstatico1"
+﻿//importScripts("/js/pouchDB.js")
+
+
+var nombreCacheEstatico = "cacheEstatico1"
 var nombreCacheDinamico = "cacheDinamico1"
 var archivosEstaticos = [
 	"/css/menu.css", "/MiProgressApp.styles.css", "/lib/jquery/dist/jquery.min.js",
 	"/lib/bootstrap/dist/js/bootstrap.bundle.min.js", "/js/menu.js", "/", "/js/generic.js", "/img/loading.gif",
-	"/Persona/listarPersonas", "/PaginaError/Index","/js/sweetalert.js"
+	"/Persona/listarPersonas", "/PaginaError/Index", "/js/sweetalert.js", "/js/pouchDB.js"
 ]
 
 self.addEventListener("install", event => {
@@ -25,30 +28,60 @@ self.addEventListener("activate", event => {
 })
 
 self.addEventListener("fetch", event => {
+	if (event.request.method != "POST") {
+		const respuesta =
+			fetch(event.request).then(response => {
+				caches.open(nombreCacheDinamico).then(cache => {
+					cache.put(event.request, response)
+				})
+				return response.clone();
+			}).catch(err => {
 
-	const respuesta = fetch(event.request).then(response => {
-		caches.open(nombreCacheDinamico).then(cache => {
-			cache.put(event.request, response)
-		})
-		return response.clone();
-	}).catch(err => {
+				return caches.match(event.request).then(res => {
 
-		return caches.match(event.request).then(res => {
-
-			if (res) return res;
-			else {
-				if (event.request.headers.get("accept").includes("text/html")) {
-					return caches.match("/PaginaError/Index")
-				} else {
-					var response = new Response('<h1 class="text-danger">Para realizar esta accion necesita internet</h1>', {
-						headers: {
-							"Content-Type": "text/html"
+					if (res) return res;
+					else {
+						if (event.request.headers.get("accept").includes("text/html")) {
+							return caches.match("/PaginaError/Index")
+						} else {
+							var response = new Response(`
+			<h1 class="text-danger">Para realizar esta accion necesita internet</h1>
+		`, {
+								headers: {
+									"Content-Type": "text/html"
+								}
+							})
+							return response;
 						}
-					})
-				}
+					}
+
+				})
 			})
-	})
-	event.respondWith(respuesta)
+
+		event.respondWith(respuesta)
+	} else {
+
+		var respuesta = fetch(event.request.clone()).then(response => {
+			if (response) return response
+
+		}).catch(err => {
+			return event.request.clone().formData().then(formdata => {
+
+				//console.log(formdata)
+				console.log(Object.fromEntries(formdata))
+				//var db = new PouchDB("BDBiblioteca")
+				//var objeto = Object.fromEntries(formdata)
+				////_id
+				//objeto._id = new Date().toISOString()
+				//objeto.url = event.request.url
+				//db.put(objeto).then(res => {
+				//	console.log("Se guardo correctamente en Index DB")
+				//})
+			})
+		})
+		event.respondWith(respuesta)
+	}
+
 })
 
 
